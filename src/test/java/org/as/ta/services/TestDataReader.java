7dca635520940821bfb3f49f0b17109dba34aa3c
andrijzaz <andrijzaz@gmail.com>
2022-09-07 17:13:43 +0300

    Version 4 of TestDataReader

--- src/test/java/org/as/ta/services/TestDataReader.java
+++ src/test/java/org/as/ta/services/TestDataReader.java
@@ -4,7 +4,7 @@ import java.util.ResourceBundle;
 
 public class TestDataReader {
     private static final ResourceBundle resourceBundle = ResourceBundle.getBundle(System
-            .getProperty("src/test/resources/environment"));
+            .getProperty("environment"));
 
     public static String getTestData(String key){
         return resourceBundle.getString(key);
